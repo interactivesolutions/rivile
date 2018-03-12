2fd46c18130327dcb7df2fa0c6ae8f4254755e11
vytautas <vytautas.rimeikis@gmail.com>
2018-03-12 10:33:37 +0200

    add get i07 by kodasPO function

--- src/Repositories/I07PardRepository.php
+++ src/Repositories/I07PardRepository.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace InteractiveSolutions\Rivile\Repositories;
 
 
+use Illuminate\Support\Collection;
 use InteractiveSolutions\HoneycombCore\Repositories\Repository;
 use InteractiveSolutions\Rivile\Models\Rivile\I07Pard;
 
@@ -21,5 +22,17 @@ class I07PardRepository extends Repository
     public function model(): string
     {
         return I07Pard::class;
+    }
+
+    /**
+     * @param string $kodasPo
+     * @return Collection
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function getByKodasPo(string $kodasPo): Collection
+    {
+        return $this->makeQuery()
+            ->where('I07_KODAS_PO', '=', $kodasPo)
+            ->get();
     }
 }
