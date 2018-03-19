469edfe489eb4da48d45c8b1eb68b679c68b0b58
vytautas <vytautas.rimeikis@gmail.com>
2018-03-19 11:22:09 +0200

    save I05 row on getting I04 data

--- src/Console/Commands/Import/ImportPayments.php
+++ src/Console/Commands/Import/ImportPayments.php
@@ -5,7 +5,8 @@ declare(strict_types = 1);
 namespace InteractiveSolutions\Rivile\Console\Commands\Import;
 
 use InteractiveSolutions\Rivile\Console\Commands\RivileCore;
-use InteractiveSolutions\Rivile\Models\Rivile\I04Ath;
+use InteractiveSolutions\Rivile\Repositories\I04AthRepository;
+use InteractiveSolutions\Rivile\Repositories\I05AtdRepository;
 
 /**
  * Class ImportPayments
@@ -25,18 +26,38 @@ class ImportPayments extends RivileCore
      * @var string
      */
     protected $description = 'GET_I04_LIST - Import';
+    /**
+     * @var I04AthRepository
+     */
+    private $athRepository;
+    /**
+     * @var I05AtdRepository
+     */
+    private $atdRepository;
+
+    /**
+     * ImportPayments constructor.
+     * @param I04AthRepository $athRepository
+     */
+    public function __construct(I04AthRepository $athRepository, I05AtdRepository $atdRepository)
+    {
+        parent::__construct();
+
+        $this->athRepository = $athRepository;
+        $this->atdRepository = $atdRepository;
+    }
 
     /**
      * Initializing data
      */
     protected function init()
     {
-        $latItem = I04Ath::orderBy('created_at', 'desc')->first();
+        $latItem = null;
 
-        $this->listType = 'H';
+        $this->listType = 'A';
         $this->filters = "I04_KODAS_CH>'$latItem'";
         $this->action = 'I04';
-        $this->xmlRootName = 'i04_ath';
+        $this->xmlRootName = 'I04';
         $this->actionMethod = self::ACTION_METHOD_GET;
     }
 
@@ -48,10 +69,19 @@ class ImportPayments extends RivileCore
     {
         $lastItem = null;
 
+        if (!isset($response[0])) {
+            $response = [$response];
+        }
+
         foreach ($response as $item) {
             $lastItem = $item;
             $this->clearEmptySpaces($item);
-            I04Ath::updateOrCreate(['I04_KODAS_CH' => $item['I04_KODAS_CH']], $item);
+            $this->athRepository->updateOrCreate([
+                'I04_KODAS_CH' => $item['I04_KODAS_CH'],
+            ], $item
+            );
+
+            $this->saveI05(array_get($item, 'I05'));
         }
 
         $this->info('Added: ' . sizeof($response));
@@ -59,6 +89,28 @@ class ImportPayments extends RivileCore
         if (sizeof($response) == 100) {
             $this->filters = "I04_KODAS_CH>'" . $lastItem['I04_KODAS_CH'] . "'";
             $this->makeCall();
+        }
+    }
+
+    /**
+     * @param array|null $data
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    private function saveI05(array $data = null)
+    {
+        if (!$data) {
+            return;
+        }
+
+        if (!isset($data[0])) {
+            $data = [$data];
+        }
+
+        foreach ($data as $item) {
+            $this->atdRepository->updateOrCreate([
+                'I05_KODAS_CH' => $item['I05_KODAS_CH'],
+                'I05_EIL_NR' => $item['I05_EIL_NR'],
+            ], $item);
         }
     }
 }
--- src/Console/Commands/Update/UpdatePayments.php
+++ src/Console/Commands/Update/UpdatePayments.php
@@ -5,7 +5,8 @@ declare(strict_types = 1);
 namespace InteractiveSolutions\Rivile\Console\Commands\Update;
 
 use InteractiveSolutions\Rivile\Console\Commands\RivileCore;
-use InteractiveSolutions\Rivile\Models\Rivile\I04Ath;
+use InteractiveSolutions\Rivile\Repositories\I04AthRepository;
+use InteractiveSolutions\Rivile\Repositories\I05AtdRepository;
 
 /**
  * Class UpdatePayments
@@ -25,18 +26,40 @@ class UpdatePayments extends RivileCore
      * @var string
      */
     protected $description = 'GET_I04_LIST - Update payments list';
+    /**
+     * @var I04AthRepository
+     */
+    private $athRepository;
+    /**
+     * @var I05AtdRepository
+     */
+    private $atdRepository;
+
+    /**
+     * UpdatePayments constructor.
+     * @param I04AthRepository $athRepository
+     * @param I05AtdRepository $atdRepository
+     */
+    public function __construct(I04AthRepository $athRepository, I05AtdRepository $atdRepository)
+    {
+        parent::__construct();
+
+        $this->athRepository = $athRepository;
+        $this->atdRepository = $atdRepository;
+    }
 
     /**
      * Initializing data
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     protected function init()
     {
-        $latItem = I04Ath::orderBy('I04_R_DATE', 'desc')->get()[1]->I04_R_DATE;
+        $latItem = $this->athRepository->makeQuery()->orderBy('I04_R_DATE', 'desc')->get()[0]->I04_R_DATE;
 
-        $this->listType = 'H';
+        $this->listType = 'A';
         $this->filters = "I04_R_DATE>'$latItem'";
         $this->action = 'I04';
-        $this->xmlRootName = 'i04_ath';
+        $this->xmlRootName = 'I04';
         $this->actionMethod = self::ACTION_METHOD_GET;
     }
 
@@ -55,7 +78,11 @@ class UpdatePayments extends RivileCore
         foreach ($response as $item) {
             $lastItem = $item;
             $this->clearEmptySpaces($item);
-            I04Ath::updateOrCreate(['I04_KODAS_CH' => $item['I04_KODAS_CH']], $item);
+            $this->athRepository->updateOrCreate([
+                'I04_KODAS_CH' => $item['I04_KODAS_CH'],
+            ], $item);
+
+            $this->saveI05(array_get($item, 'I05'));
         }
 
         $this->info('Updated / added: ' . sizeof($response));
@@ -63,6 +90,28 @@ class UpdatePayments extends RivileCore
         if (sizeof($response) == 100) {
             $this->filters = "I04_R_DATE>'" . $lastItem['I04_R_DATE'] . "'";
             $this->makeCall();
+        }
+    }
+
+    /**
+     * @param array|null $data
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    private function saveI05(array $data = null)
+    {
+        if (!$data) {
+            return;
+        }
+
+        if (!isset($data[0])) {
+            $data = [$data];
+        }
+
+        foreach ($data as $item) {
+            $this->atdRepository->updateOrCreate([
+                'I05_KODAS_CH' => $item['I05_KODAS_CH'],
+                'I05_EIL_NR' => $item['I05_EIL_NR'],
+            ], $item);
         }
     }
 }
